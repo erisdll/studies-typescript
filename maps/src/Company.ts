47d351a0efdd6f6a16b23b258f246af0a33820c5
erisdll <72031082+erisdll@users.noreply.github.com>
2023-11-02 21:58:41 -0300

    implemented mappable interface

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,19 +1,30 @@
-import { faker } from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
+import { Mappable } from './CustomMap';
 
-export class Company {
-    companyName: string;
-    catchPhrse: string;
-    location: {
-        lat: number;
-        lng: number;
-    }
+export class Company implements Mappable {
+  companyName: string;
+  catchPhrse: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: string;
 
-    constructor() {
-        this.companyName = faker.company.name();
-        this.catchPhrse = faker.company.catchPhrase();
-        this.location = {
-            lat: faker.location.latitude(),
-            lng: faker.location.longitude(),
-        };
-    }
+  constructor() {
+    this.companyName = faker.company.name();
+    this.catchPhrse = faker.company.catchPhrase();
+    this.location = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude(),
+    };
+    this.color = faker.color.human();
+  }
+  markerContent(): string {
+    return `
+    <div>
+    <h1>Company name ${this.companyName}
+    <h3>Catchphrase: ${this.catchPhrse}
+    </div>
+    `;
+  }
 }
