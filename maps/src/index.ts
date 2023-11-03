c251a48ed0122389e9ce4155b7fd19df786cf57f
erisdll <72031082+erisdll@users.noreply.github.com>
2023-11-02 21:59:22 -0300

    Completed Section 9: Design Patterns

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,14 +1,10 @@
-/// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 const company = new Company();
+const customMap = new CustomMap('map');
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 10,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+customMap.addMarker(user);
+customMap.addMarker(company);
