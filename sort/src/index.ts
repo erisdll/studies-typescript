5e803d8e4c65de2cb580142c3cc4e723763d63bd
erisdll <72031082+erisdll@users.noreply.github.com>
2023-11-07 23:22:25 -0300

    classes refactor for extension

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,20 +1,23 @@
-import { Sorter } from './sorter';
+import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharacterCollection } from './CharacterCollection';
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([1 / 3, 33, 50000, 10]);
-const numSorter = new Sorter(numbersCollection);
-numSorter.sort();
-console.log(numSorter.collection);
+numbersCollection.sort();
 
-const charactersCollection = new CharacterCollection('abcdefghijklmnopqrstuvwxyz');
-const charSorter = new Sorter(charactersCollection);
-charSorter.sort();
-console.log(charSorter.collection);
+const charactersCollection = new CharacterCollection(
+  'abcdefghijklmnopqrstuvwxyz'
+);
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
-const linkedList = new LinkedList()
-linkedList.add(1)
-linkedList.add(5)
-linkedList.add(4)
-console.log(linkedList);
+const linkedList = new LinkedList();
+linkedList.add(200);
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.add(13);
+linkedList.sort();
+linkedList.print();
